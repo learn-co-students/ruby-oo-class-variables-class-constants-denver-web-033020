55bd72d0b59dc93c003780e9ab06323c36ebb3d7
LisaHJung <lisahjung@gmail.com>
2020-04-05 09:58:26 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,19 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+# Objective : keep track of all of the genres of all of the Books we create using a class constant.
+# 1 Create contant GENRES and set it equal to an empty array. This is where we will store the genres of all instances. 
+# 2 We know that we need to somehow get the genre of each instance and shovel it in the class constant GENRES
+# 2 Judging by the original code, we know that genre is not one of the attributes initialized when an instance is created
+# 3 In original, attr_accessor contained :author, :page_count, AND :genre, meaning genre has been set and can be gotten.
+# 3 However, you know that we need to customize the genre method as we want to add the genre into the class constant GENRES.
+    # 3 We will need to create our customed setter method so delete :genres from attr_accessor and in clude in attr_ reader. we will still want to get this method
+# 4 define genre where you set the genre then, shovel it into the constants array. 
 
-  def initialize(title)
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre #3
+  
+  GENRES =[] # 1
+
+  def initialize(title) #2
     @title = title
   end
 
@@ -10,4 +21,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre) #4
+    @genre = genre
+    GENRES << genre
+  end
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,19 @@
+# Objective: Keep track of all of the brands of the all instances of shoe
+# 1 Very similar to book.rb however, brand initialized upon instantiation of shoe. 
+# 2 You also see that attr_reader for :brand has already been set up as well, meaning that we will make customization to initialize method directly.
+# 3 Create a class constant BRANDS and set it equal to an empty array.
+# 4 Shovel the brand into the class constant brand.
+  # notice that we are being asked to have an array that only contains one instance of a genre, no repeats
+    # constant BRAND is an array so we apply.include? method. we preface it by brand unless to say shovel brand in unless the class constant BRAND array inclues that brand already. 
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand #2
 
-  def initialize(brand)
-    @brand = brand
+  BRANDS = [] #3
+
+  def initialize(brand) #1
+    @brand = brand #2 
+    BRANDS << brand unless BRANDS.include?(brand) #4
   end
 
   def cobble
